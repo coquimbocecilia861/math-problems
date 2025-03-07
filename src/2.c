f703f605881d31fa6865de1bf29dea7ac785e9ba
coquimbocecilia861 <coquimbocecilia861@gmail.com>
2025-03-07 14:01:32 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,12 @@
+
+// C Program to Find the Sum of Two Numbers
+
+#include <stdio.h>
+int main()
+{
+    int num1 = 5;
+    int num2 = 10;
+    int sum = num1 + num2;
+    printf("The sum is: %d", sum);
+    return 0;
+}
