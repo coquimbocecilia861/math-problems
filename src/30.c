0b9e8d9629840c4551103fb7289d9c41b15ba39c
coquimbocecilia861 <coquimbocecilia861@gmail.com>
2025-04-07 05:17:18 +0000

    Add 30.c

---
+++ src/30.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int i = 0;
+    while(i < 5) {
+        if (i % 2 == 0)
+            printf("Even\n");
+        else
+            printf("Odd\n");
+        i++;
+    }
+}
